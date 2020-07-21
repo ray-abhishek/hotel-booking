2595f90afc4100b98a3ee4d3c9a4d5eef32f59ee
Abnish <akp12556@gmail.com>
2020-07-21 21:53:39 +0530

    setting a folder structure

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './commonNavbar/Navbar';
+import Footer from './commonNavbar/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
      hello
-
+    <Footer />
     </div>
   );
 }
