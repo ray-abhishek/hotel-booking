961b7f21dc072e0c2b44374ceb8a753321804429
Abhishek Ray <apostate22@gmail.com>
2020-07-24 00:54:52 +0530

    Imported all the TableModels

---
+++ src/server/app/main/models/__init__.py
@@ -0,0 +1,6 @@
+""" import all the models in the folder here"""
+from . import UserModel
+from . import AuthUserModel
+from . import EntityModel
+from . import PolicyModel
+from . import HomeTruthModel
