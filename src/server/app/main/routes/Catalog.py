6e7da32c79b5c6ed198f2393de397c3abfcacc3d
Abhishek Ray <apostate22@gmail.com>
2020-07-24 00:56:21 +0530

    Route for Catalog URL

---
+++ src/server/app/main/routes/Catalog.py
@@ -0,0 +1,42 @@
+from flask import request
+from flask_restful import Resource, reqparse
+from app.main import db
+from app.main.services.catalog import get_catalog_data
+import jwt
+from app.main.settings import key
+import datetime
+
+# Route for Catalog
+class Catalog(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('arrivalDate', type=str,required=False)
+    parser.add_argument('departureDate', type=str,required=False)
+    parser.add_argument('sleeps', type=str,required=False)
+    parser.add_argument('perpage', type=str,required=False)
+    parser.add_argument('sort', type=str,required=False)
+    parser.add_argument('page', type=str,required=False)
+    parser.add_argument('feature', type=str,required=False, action='append')
+    
+    parser.add_argument('max_price', type=str,required=False)
+    parser.add_argument('min_price', type=str,required=False)
+
+
+    @classmethod
+    def get(self, location=None):
+        data = Catalog.parser.parse_args()
+        print("\n\n---INSIDE GET Catalog---\n")
+        print(location," is location")
+        if location:
+            data["location"] = location
+        print(data," are the parameters passed to Catalog")
+        params = request.args
+        print(params," are params sent for fetching catalog data")
+        flag, total_results, total_pages, catalog_data = get_catalog_data(data)
+
+        if flag:
+            return {"status" : "success", "data" : catalog_data, "totalresults" : total_results, "totalpages" : total_pages}
+        else:
+            return {"status" : "failure" , "message" : "Could not fetch results. Please try again."}
+
+
+
