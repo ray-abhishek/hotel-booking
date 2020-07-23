df63b59958fd2b5575bb28392aa147ebf912afe0
Abhishek Ray <apostate22@gmail.com>
2020-07-24 00:54:30 +0530

    Table to store User Details

---
+++ src/server/app/main/models/UserModel.py
@@ -0,0 +1,13 @@
+from .. import db
+import datetime
+
+class UserModel(db.Model):
+    
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, default=datetime.datetime.now())
+    name = db.Column(db.String(70), nullable=False)
+    email = db.Column(db.String(70), nullable=False)
+    password = db.Column(db.String(255), nullable=True)
