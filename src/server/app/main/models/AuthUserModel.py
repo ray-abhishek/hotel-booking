b3a9d94777a33a5435f48b5a179408c016869134
Abhishek Ray <apostate22@gmail.com>
2020-07-24 00:52:37 +0530

    Created Table to store Google Logins

---
+++ src/server/app/main/models/AuthUserModel.py
@@ -0,0 +1,11 @@
+from .. import db
+import datetime
+
+class AuthUserModel(db.Model):
+    
+    __tablename__ = 'authusers'
+
+    id = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    email = db.Column(db.String(70), nullable=False)
+    profile_pic = db.Column(db.String(70), nullable=False)
