3a0b3aaf4b868424ff04bea8e2da928509ffed61
Abhishek Ray <apostate22@gmail.com>
2020-07-24 17:28:13 +0530

    Added Gallery Component to Catalog.js

--- src/client/src/pages/Catalog.js
+++ src/client/src/pages/Catalog.js
@@ -2,12 +2,14 @@ import React from "react";
 import Filter from "../components/catalog/Filter";
 import Sort from "../components/catalog/Sort";
 import Pagination from "../components/catalog/Pagination";
+import Gallery from "../components/catalog/Gallery";
 
 function Catalog(props) {
   return (
     <div>
       <Filter {...props} />
       <Pagination {...props} />
+      <Gallery {...props}/>
     </div>
   );
 }
