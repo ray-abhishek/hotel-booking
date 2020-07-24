796f49576e2372af3dba52a314ec0608c162e958
Abhishek Ray <apostate22@gmail.com>
2020-07-24 16:47:26 +0530

    Made Nullable to True for created_at

--- src/server/app/main/models/UserModel.py
+++ src/server/app/main/models/UserModel.py
@@ -7,7 +7,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True),
-                           nullable=False, default=datetime.datetime.now())
+                           nullable=True, default=datetime.datetime.now())
     name = db.Column(db.String(70), nullable=False)
     email = db.Column(db.String(70), nullable=False)
     password = db.Column(db.String(255), nullable=True)
