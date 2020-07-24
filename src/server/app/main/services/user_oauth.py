ff156dd33cd62c0fe9e799cb03f985bf334ee668
Abhishek Ray <apostate22@gmail.com>
2020-07-24 16:51:01 +0530

    Methods to handle login from oAuth providers like google.

--- src/server/app/main/services/user_oauth.py
+++ src/server/app/main/services/user_oauth.py
@@ -1,23 +1,54 @@
-from flask_login import UserMixin
+from ..models.UserModel import UserModel
 from ..models.AuthUserModel import AuthUserModel
-from ..utils.save_data import save_changes
 from app.main import db
+import jwt
+import datetime
+from app.main.settings import key
+from ..utils.save_data import save_changes
+
+#Function for login of user
+def oauth_login(data):
+    
+    data_raw=UserModel().query.filter(UserModel.email == data["email"]).first()
+
+    flag=False
+
+    # check if data_raw is NoneType. 
+    if not data_raw:
+
+        new_asset = AuthUserModel(id = data["googleId"],
+                            name = data['name'],
+                            email = data['email'],
+                            profile_pic = data['imageUrl']
+                            )
+
+        save_changes(new_asset)
+
+        new_asset = UserModel(name = data['name'],
+                            email = data['email'],
+                            )
 
 
-def getAuthUser(user_id):
+        save_changes(new_asset)
+        user_id = new_asset.id
+
         
-    user = AuthUserModel.query.filter_by(AuthUserModel.id == user_id).first()
-    return user
+    data_raw=UserModel().query.filter(UserModel.email == data["email"]).first()
 
 
-def createAuthUser(user_id, name, email, profile_pic_):
+    # check if data_raw is not NoneType
+    if data_raw:
+        payload={
+            "email":data["email"],
+            "created_at":str(datetime.datetime.utcnow()),
+            "exp_datetime":str(datetime.datetime.utcnow() + datetime.timedelta(days=1))
+        }
+
+        token = jwt.encode(payload,key,algorithm="HS256")
         
-    new_asset = AuthUserModel(id = user_id, 
-                            name = name,
-                            email = email,
-                            profile_pic = profile_pic_)
-                            
-    save_changes(new_asset)
+        return True, token.decode(), data_raw.name
 
+    else:
+        return False, "", ""
         
         
