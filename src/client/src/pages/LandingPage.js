7b806820ea4abb64f16172a73618e1e354cde905
Abnish <akp12556@gmail.com>
2020-07-25 11:24:08 +0530

    Landing page

---
+++ src/client/src/pages/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '../components/landingPage/Header';
+
+class LandingPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <>
+            
+            <Header />
+            </>
+        )
+    }
+}
+
+export default LandingPage;
